851a287eaabe596928aaa37b22b23f7316739b2f
Alexandrafirigua <126630042+Alexandrafirigua@users.noreply.github.com>
2023-06-27 00:36:52 -0500

    agregar imagenes

--- TurismoFront/src/app/app-routing.module.ts
+++ TurismoFront/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { DescubreComponentComponent } from './components/descubre-component/desc
 import { EditarProductosComponent } from './components/editar-productos/editar-productos.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { DesubreMasComponent } from './components/desubre-mas/desubre-mas.component';
+import { BogotaPaginaComponent } from './components/bogota-pagina/bogota-pagina.component';
 
 
 
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'editar', component: EditarProductosComponent},
   { path: 'registrate', component: DescubreComponentComponent},
   { path: 'descubre-mas', component: DesubreMasComponent},
+  { path: 'bogota-pagina', component: BogotaPaginaComponent},
   { path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
